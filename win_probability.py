07dea974d44e673237520e5e1df4c25b9790c98f
hokiespider <andy.krukiel@gmail.com>
2020-11-12 01:24:32 -0600

    Uploading document that breaks out the lines correctly.  Still bugs figuring out wins and losses

--- win_probability.py
+++ win_probability.py
@@ -6,46 +6,46 @@ import requests
 import json
 
 
-def jstring(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    return text
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-def sign(x):
-  return 1-(x<=0)
-
-
 # Get data from the API
 df = pd.DataFrame()
-for x in range(2013, 2021, 1):    # Line data is only available 2013+
+for x in range(2013, 2020, 1):    # Line data is only available 2013+
     parameters = {
-        "conference": "ACC",
+        "team": "Virginia Tech",
         "year": x
     }
     response = requests.get("https://api.collegefootballdata.com/lines", params=parameters)
 
     # Import the data into a pandas DataFrame
-    json_str = jstring(response.json())
-    temp = pd.read_json(json_str)
+    temp = pd.DataFrame(response.json()) # Create a DataFrame with a lines column that contains JSON
+    # need to fill NA line lists for 2020
+    temp = temp.explode('lines') # Explode the DataFrame so that each line gets its own row
+    temp = temp.reset_index(drop=True) # After explosion, the indices are all the same - this resets them so that you can align the DataFrame below cleanly
+
+    lines_df = pd.DataFrame(temp.lines.tolist()) # A separate lines DataFrame created from the lines JSON column
+    temp = pd.concat([temp, lines_df], axis=1) # Concatenating the two DataFrames along the vertical axis.
+
     df = df.append(temp)
+df = df[df.provider == 'consensus']
+school = "Virginia Tech"
 
 # explode the 4 line providers into their own rows
 #df = df.explode('lines')
 
+
+
 # Add Win/Loss column
-school = "Virginia Tech"
-home_games = df.query('homeTeam == @school')
+
+df['score_diff'] = df['homeScore'] - df['awayScore']
+df.loc[]
+
+
+home_games = df[df.homeTeam == school]
 home_games['score_diff'] = home_games['homeScore'] - home_games['awayScore']
 home_games['home_away'] = "Home"
 home_games.loc[home_games['score_diff'] > 0, 'wins'] = 1
 home_games.loc[home_games['score_diff'] < 0, 'losses'] = 1
 
 away_games = df.query('awayTeam == @school')
-df.info()
 away_games['score_diff'] = away_games['awayScore'] - away_games['homeScore']
 away_games['home_away'] = "away"
 away_games.sample()
