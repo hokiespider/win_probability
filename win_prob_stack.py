e2057690104e7ae5ccae7fd78e73562a2f1b6de2
hokiespider <andy.krukiel@gmail.com>
2020-11-12 01:24:49 -0600

    Uploading code from stack overflow to fix the line columns

---
+++ win_prob_stack.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import requests
+
+params = {
+    "gameId": 401112435,
+    "year": 2019,
+}
+
+r = requests.get("https://api.collegefootballdata.com/lines", params=params)
+
+df = pd.DataFrame(r.json()) # Create a DataFrame with a lines column that contains JSON
+df = df.explode('lines') # Explode the DataFrame so that each line gets its own row
+df = df.reset_index(drop=True) # After explosion, the indices are all the same - this resets them so that you can align the DataFrame below cleanly
+
+lines_df = pd.DataFrame(df.lines.tolist()) # A separate lines DataFrame created from the lines JSON column
+df = pd.concat([df, lines_df], axis=1) # Concatenating the two DataFrames along the vertical axis.
+
+# Now you can filter down to whichever rows you need.
+df = df[df.provider == 'consensus']
